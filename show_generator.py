c484b0c6e07c0afede43f7311bd06885a5da0765
fuchami <famichiki.yuuki@gmail.com>
2018-12-25 13:48:12 +0900

    show generate image

---
+++ show_generator.py
@@ -0,0 +1,40 @@
+# coding:utf-8
+"""
+ImageDataGeneratorで生成された画像を可視化
+
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+import load
+
+def show_img(imgs, row, col):
+    if len(imgs) != (row*col):
+        raise ValueError("Invalid imgs len:{} col:{} row:{}".format(len(imgs), row, col))
+    
+    fig = plt.figure(figsize=(8, 8))
+    fig.subplots_adjust(hspace=0.05, wspace=0.05)
+    for i, img in enumerate(imgs):
+        plot_num = i+1
+        ax = fig.add_subplot(row, col, plot_num, xticks=[], yticks=[])
+        ax.imshow(img)
+    plt.show()
+
+
+if __name__ == "__main__":
+    classes = ['bisco','clearclean', 'frisk', 'toothbrush', 'udon']
+    datagen = load.MyImageDataGenerator(
+                        rescale=1/255.0,
+                        mix_up_alpha=4)
+    
+    max_img_num = 12
+    imgs = []
+    for d in datagen.flow_from_directory('../DATASETS/compare_dataset/train_full/',
+                                            batch_size=1, target_size=(128,128),
+                                            class_mode='categorical',
+                                            ):
+        imgs.append(np.squeeze(d[0], axis=0))
+        if (len(imgs) % max_img_num) == 0:
+            break
+    
+    show_img(imgs, row=4, col=3)
